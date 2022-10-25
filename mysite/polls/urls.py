c4453a644e6ee402bc50da2c94cbaa647aa1f3ae
DedPoleno <115897004+DedPoleno@users.noreply.github.com>
2022-10-26 02:36:47 +0300

    Add files via upload

---
+++ mysite/polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    # Так, эту херь я сделал в субботу по 3 уроку из документации, и доделал по 4 уроку в ту же субботу
+]
